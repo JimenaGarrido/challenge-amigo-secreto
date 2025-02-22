44a268a4cf87ff6292f1654b3a6501aa324cf339
Jimena <jimenagarrido332@gmail.com>
2025-02-21 21:08:52 -0600

    Crear función agregarAmigos a la app.js

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ function asignarTextoId(id, texto) {
     let elemento = document.getElementById(id);
     elemento.innerHTML = texto;
 }
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre !== "") {
+        console.log(nombre);
+        amigos.push(nombre);
+        console.log(amigos);
+        mostrarLista();
+    } else {
+        alert("Por favor, ingrese un nombre válido");
+    }
+}
